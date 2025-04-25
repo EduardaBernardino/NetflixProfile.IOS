3750fae66e98fdceade8dcb2abcc91e5da63b2f6
Eduarda Bernardino <eduardabernardinoo06@gmail.com>
2025-04-25 14:15:45 -0300

    Create TabView.App.swift

---
+++ TabView.App.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+struct TabView_App: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TabView_App()
+}
