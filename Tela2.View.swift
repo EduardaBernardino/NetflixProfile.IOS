55742192b7ec4a931948d70859a6c4c4e65dba83
Eduarda Bernardino <eduardabernardinoo06@gmail.com>
2025-04-25 14:17:19 -0300

    Create Tela2.View.swift

---
+++ Tela2.View.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+struct Tela2_View: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Tela2_View()
+}
